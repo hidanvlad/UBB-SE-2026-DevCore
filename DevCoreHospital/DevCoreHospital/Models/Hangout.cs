67bd5b61d69b37d757b528d823081b5d87a73249
MarchisPatrick04 <patrick.marchis@stud.ubbcluj.ro>
2026-03-24 13:15:47 +0200

    I created the Hangout entity class, with the HangoutRepository class (but only with the constructor)

---
+++ DevCoreHospital/DevCoreHospital/Models/Hangout.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+namespace DevCoreHospital.Models
+{
+    public class Hangout
+    {
+        public int hangoutID { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public DateAndTime date { get; set; }
+        public int maxParticipants { get; set; }
+        public List<Staff> participantList { get; set; }
+
+        public Hangout(int hangoutID, string title, string description, DateAndTime date, int maxParticipants)
+        {
+            this.hangoutID = hangoutID;
+            this.title = title;
+            this.description = description;
+            this.date = date;
+            this.maxParticipants = maxParticipants;
+            this.participantList = new List<Staff>(this.maxParticipants);
+        }
+    }
+}
---
+++ DevCoreHospital/DevCoreHospital/Repositories/HangoutRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevCoreHospital.Models;
+
+namespace DevCoreHospital.Repositories
+{
+    public class HangoutRepository
+    {
+        public List<Hangout> hangoutList;
+
+        public HangoutRepository()
+        {
+            hangoutList = new List<Hangout> ();
+        }
+
+        /// the other methods are assigned to Laszlo
+    }
+}
