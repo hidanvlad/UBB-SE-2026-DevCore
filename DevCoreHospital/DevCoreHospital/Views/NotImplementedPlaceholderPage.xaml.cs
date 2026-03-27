80d81730df3f8e0aab08368292739f565eb5f0b1
HulubeacSebastian <sebastian.hulubeac@stud.ubbcluj.ro>
2026-03-27 10:09:34 +0200

    solve error

---
+++ DevCoreHospital/DevCoreHospital/Views/NotImplementedPlaceholderPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.UI.Xaml.Controls;
+
+namespace DevCoreHospital.Views
+{
+    public sealed partial class NotImplementedPlaceholderPage : Page
+    {
+        public NotImplementedPlaceholderPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
